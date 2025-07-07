decb547ad9fdcb359bf98203848218e6c4e2ca7d
Julien Etienne <julien.etienne@intuiquest.com>
2025-07-07 23:13:35 +0700

    update tiles

--- js/config.js
+++ js/config.js
@@ -1,11 +1,16 @@
+// Auto-generated config file
 export const clipBounds = {
-  minLat: 16.0,   // Slightly extended south to cover below Khon Kaen
-  maxLat: 20.5,   // Still up to Mae Sai
-  minLon: 97.5,   // Western edge (Mae Sot)
-  maxLon: 103.0   // Extended east to include Khon Kaen
+  "minLat": 16.5,
+  "maxLat": 20.5,
+  "minLon": 97.5,
+  "maxLon": 103
 };
 
 export const regionBounds = [
   [clipBounds.minLat, clipBounds.minLon],
   [clipBounds.maxLat, clipBounds.maxLon]
 ];
+
+export const minZoom = 10;
+export const maxZoom = 13;
+  
